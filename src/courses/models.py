f4315dc9ca2eb2e6bec33c4366e382bf8538d88f
nellyXinwei <nelsonalbajr@yahoo.com>
2020-07-07 23:48:51 +0800

    FINISHED

--- src/courses/models.py
+++ src/courses/models.py
@@ -5,3 +5,6 @@ from django.urls import reverse
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=120)
+
+    def get_absolute_url(self):
+        return reverse('courses:course-detail', kwargs={'id': self.id})
--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from courses.views import (
-    CourseView
+    CourseView,
+    CourseListView,
+    CourseCreateView,
+    CourseUpdateView,
+    CourseDeleteView
 )
 
 app_name = 'courses'
 urlpatterns = [
-    path('',CourseView.as_view(template_name='contact.html'), name='courses-list'),
-
-
-    #path('', courses_list_view, name='courses-list'),
-    #path('create/', courses_create_view, name='courses-create'),
-    path('<int:id>/', CourseView.as_view(), name='courses-detail'),
-    #path('<int:id>/update/', courses_update_view, name='courses-update'),
-    #path('<int:id>/delete/', courses_delete_view, name='courses-delete'),
+    path('',CourseListView.as_view(), name='course-list'),
+    path('create/', CourseCreateView.as_view(), name='course-create'),
+    path('<int:id>/', CourseView.as_view(), name='course-detail'),
+    path('<int:id>/update/', CourseUpdateView.as_view(), name='course-update'),
+    path('<int:id>/delete/', CourseDeleteView.as_view(), name='course-delete'),
 
 
 ]
